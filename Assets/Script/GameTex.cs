886670a0ca69c3f8a4a2722ca63f83c2e7563bec
caishen233 <caishen@89trillion.com>
2021-06-05 18:15:55 +0800

    first commit

--- Assets/Script/GameTex.cs
+++ Assets/Script/GameTex.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using SimpleJSON;
 
 //定义实体类，用来存放Json的数据
-class Card
+class  Card
 {
     public int ProductId;
     public int type;
@@ -14,39 +14,33 @@ class Card
     public int isPurchased;
 }
 
+
+
 public class GameTex : MonoBehaviour
 {
+    public TextAsset txt;
+    
+    public Transform contentTransform;
+
     //生成卡片的预设体
-    public GameObject freeCoinPrefab;
-    public GameObject heroCardPrefab;
-    public GameObject freeDiamondsPrefab;
-    public GameObject noCardPrefab;
+    public GameObject Prefab;
 
-    public  static string moveSpriet;
-
-
-    //四种英雄卡片对应的英雄图片
-    public Sprite subType7;
-    public Sprite subType20;
-    public Sprite subType13;
-    public Sprite subType18;
+    private string moveSpriet;
 
     //存放实体类的链表
-    private List<Card> cardList;
-     void Start()
-    {
-        InitTextList ();
-    }
+    private  List<Card> cardList;
 
-    void Update ()
+    void Start()
     {
+        moveSpriet=txt.text;
+        InitTextList();
     }
 
     //解析Json，存放到List中
     public void InitTextList()
     {
         cardList = new List<Card>();
-        var n = JSONNode.Parse (moveSpriet);
+        var n = JSONNode.Parse(moveSpriet);
         var m = n["dailyProduct"];
         for (int i = 0; i < m.Count; i++)
         {
@@ -61,101 +55,130 @@ public class GameTex : MonoBehaviour
         }
     }
 
-    //根据Json提供的数据显示英雄卡
     public void createcard()
     {
-        int listLen=cardList.Count;
-        int cardSum=0;
-        //左右上下位移prefab
-        float horizontal_move=352.0f;
-        float vertical_move=630.0f;
-
-        if(listLen%3!=0)
+        int listLen = cardList.Count;
+        Debug.Log(listLen);
+        int cardSum = 0;
+        if (listLen % 3 != 0)
         {
-            cardSum=3*(listLen/3+1)-listLen;
+            cardSum = 3 * (listLen / 3 + 1) - listLen;
         }
 
-        //循环生产卡片的Prefab和卡片信息的赋值
-        for(int i=0;i<listLen;i++)
+        for (int i = 0; i < listLen; i++)
         {
-            GameObject newGameObject;
-            if(cardList[i].type==1){
-                   newGameObject = Instantiate (		
-			       freeCoinPrefab, transform		
-                  )as GameObject;	
-                  newGameObject.transform.Translate(Vector3.left * horizontal_move);
-            }
-            if(cardList[i].type==2)
-            {
-              newGameObject = Instantiate (		
-			  freeDiamondsPrefab, 					
-			  transform		
-            )as GameObject;
-            newGameObject.transform.Translate(Vector3.left * horizontal_move);
-            }
-            if(cardList[i].type==3)
-            {
-                
-                if(cardList[i].subType==7)
-                {
-                   newGameObject = Instantiate (		
-                    heroCardPrefab, 					
-			       transform
-                   )as GameObject;
-                   Hero h=newGameObject.GetComponent<Hero>();
-                   h.heroCardImage.sprite=subType7;
-                   h.Etext(cardList[i].costGold.ToString());
-                }
-                if(cardList[i].subType==20)
-                {
-                   newGameObject = Instantiate (		
-			       heroCardPrefab, 					
-			       transform
-                   )as GameObject;
-                   newGameObject.transform.Translate(Vector3.right * horizontal_move);
-                   Hero h=newGameObject.GetComponent<Hero>();
-                   h.Etext(cardList[i].costGold.ToString());
-                   h.heroCardImage.sprite=subType20;
-                }
-                if(cardList[i].subType==13)
-                {
-                   newGameObject = Instantiate (		
-			       heroCardPrefab, 					
-			       transform
-                   )as GameObject;
-                   newGameObject.transform.Translate(Vector3.left * horizontal_move);
-                   newGameObject.transform.Translate(Vector3.down * vertical_move);
-                   Hero h=newGameObject.GetComponent<Hero>();
-                   h.Etext(cardList[i].costGold.ToString());
-                   h.heroCardImage.sprite=subType13;
-                }
-
-                if(cardList[i].subType==18)
-                {
-                    newGameObject = Instantiate (		
-			       heroCardPrefab, 					
-			       transform
-                   )as GameObject;
-                   newGameObject.transform.Translate(Vector3.down * vertical_move);
-                   Hero h=newGameObject.GetComponent<Hero>();
-                   h.Etext(cardList[i].costGold.ToString());
-                   h.heroCardImage.sprite=subType18;
-                }
-            }
+            GameObject newGameObject = Instantiate(Prefab, contentTransform) as GameObject;
+            Hero h1 = newGameObject.GetComponent<Hero>();
+            Debug.Log(cardList[i].type);
+            Debug.Log(cardList[i].subType);
+            Debug.Log(cardList[i].costGold);
+            h1.Init(cardList[i].type, cardList[i].subType, cardList[i].costGold);
         }
-        for(int i=0;i<cardSum;i++)
+
+        for (int i = 0; i < cardSum; i++)
         {
             GameObject newGameObject;
-              newGameObject = Instantiate (		
-			  noCardPrefab, 					
-			  transform		
-            )as GameObject;
-            newGameObject.transform.Translate(Vector3.right * horizontal_move);
-            newGameObject.transform.Translate(Vector3.down * vertical_move);
+            newGameObject = Instantiate(Prefab, contentTransform) as GameObject;
+            Hero h2 = newGameObject.GetComponent<Hero>();
+            h2.Init(0,0,0);
         }
     }
-
 }
+
+// //根据Json提供的数据显示英雄卡
+    // public void createcard()
+    // {
+    //     int listLen=cardList.Count;
+    //     int cardSum=0;
+    //     //左右上下位移prefab
+    //     float horizontal_move=352.0f;
+    //     float vertical_move=630.0f;
+    //
+    //     if(listLen%3!=0)
+    //     {
+    //         cardSum=3*(listLen/3+1)-listLen;
+    //     }
+    //
+    //     //循环生产卡片的Prefab和卡片信息的赋值
+    //     for(int i=0;i<listLen;i++)
+    //     {
+    //         GameObject newGameObject;
+    //         if(cardList[i].type==1){
+    //                newGameObject = Instantiate (		
+			 //       freeCoinPrefab, transform		
+    //               )as GameObject;	
+    //               newGameObject.transform.Translate(Vector3.left * horizontal_move);
+    //         }
+    //         if(cardList[i].type==2)
+    //         {
+    //           newGameObject = Instantiate (		
+			 //  freeDiamondsPrefab, 					
+			 //  transform		
+    //         )as GameObject;
+    //         newGameObject.transform.Translate(Vector3.left * horizontal_move);
+    //         }
+    //         if(cardList[i].type==3)
+    //         {
+    //             
+    //             if(cardList[i].subType==7)
+    //             {
+    //                newGameObject = Instantiate (		
+    //                 heroCardPrefab, 					
+			 //       transform
+    //                )as GameObject;
+    //                Hero h=newGameObject.GetComponent<Hero>();
+    //                h.heroCardImage.sprite=subType7;
+    //                h.Etext(cardList[i].costGold.ToString());
+    //             }
+    //             if(cardList[i].subType==20)
+    //             {
+    //                newGameObject = Instantiate (		
+			 //       heroCardPrefab, 					
+			 //       transform
+    //                )as GameObject;
+    //                newGameObject.transform.Translate(Vector3.right * horizontal_move);
+    //                Hero h=newGameObject.GetComponent<Hero>();
+    //                h.Etext(cardList[i].costGold.ToString());
+    //                h.heroCardImage.sprite=subType20;
+    //             }
+    //             if(cardList[i].subType==13)
+    //             {
+    //                newGameObject = Instantiate (		
+			 //       heroCardPrefab, 					
+			 //       transform
+    //                )as GameObject;
+    //                newGameObject.transform.Translate(Vector3.left * horizontal_move);
+    //                newGameObject.transform.Translate(Vector3.down * vertical_move);
+    //                Hero h=newGameObject.GetComponent<Hero>();
+    //                h.Etext(cardList[i].costGold.ToString());
+    //                h.heroCardImage.sprite=subType13;
+    //             }
+    //
+    //             if(cardList[i].subType==18)
+    //             {
+    //                 newGameObject = Instantiate (		
+			 //       heroCardPrefab, 					
+			 //       transform
+    //                )as GameObject;
+    //                newGameObject.transform.Translate(Vector3.down * vertical_move);
+    //                Hero h=newGameObject.GetComponent<Hero>();
+    //                h.Etext(cardList[i].costGold.ToString());
+    //                h.heroCardImage.sprite=subType18;
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<cardSum;i++)
+    //     {
+    //         GameObject newGameObject;
+    //           newGameObject = Instantiate (		
+			 //  noCardPrefab, 					
+			 //  transform		
+    //         )as GameObject;
+    //         newGameObject.transform.Translate(Vector3.right * horizontal_move);
+    //         newGameObject.transform.Translate(Vector3.down * vertical_move);
+    //     }
+    // }
+
 
 
 
--- Assets/Script/Hero.cs
+++ Assets/Script/Hero.cs
@@ -9,25 +9,85 @@ public class Hero : MonoBehaviour
 {
     public Text heroCardCost;
     public Image heroCardImage;
-    public Button buyHeroCard;
+    public Button buyCardbuttom;
+    public Image coinIamge;
+    public Text FreeText;
+    public Text cardName;
+    public Text clockText;
+    public Image BuySucces;
 
+    public Sprite clockSprite;
+    public Sprite Coin;
+    public Sprite diamond;
+    public Sprite subType7;
+    public Sprite subType20;
+    public Sprite subType13;
+    public Sprite subType18;
     //购买之后隐藏button
-    public void Click()
+    public void BuyClick()
     {
-        buyHeroCard.gameObject.SetActive(false);
+        buyCardbuttom.gameObject.SetActive(false);
     }
 
     // Prefab英雄卡片heroCardCost赋值
-    public void Etext(string s){
-    heroCardCost.text=s;
-    }
-    void Start()
+    public void Init(int type,int subtype,int costgold)
     {
-        //transform.GetComponent<Image>().Sprite=sprite;
+        //Debug.Log(type);
+        if (type == 1)
+        {
+            heroCardImage.sprite = Coin;
+            cardName.text = "Coins";
+            clockText.gameObject.SetActive(false);
+            FreeText.gameObject.SetActive(true);
+            coinIamge.gameObject.SetActive(false);
+            //金币卡片
+        }
+
+        if (type == 2)
+        {
+            heroCardImage.sprite = diamond;
+            cardName.text = "Diamond";
+            clockText.gameObject.SetActive(false);
+            FreeText.gameObject.SetActive(true);
+            coinIamge.gameObject.SetActive(false);
+            //钻石卡片
+        }
+
+        if (type == 3)
+        {
+            //Debug.Log(type);
+            clockText.gameObject.SetActive(false);
+            heroCardCost.text = costgold.ToString();
+            cardName.text = "Viking Wattion";
+            FreeText.gameObject.SetActive(false);
+            
+            if (subtype == 7)
+            {
+                heroCardImage.sprite = subType7;
+            }
+            if (subtype == 20)
+            {
+                
+                heroCardImage.sprite=subType20;
+            }
+            if (subtype == 13)
+            {
+                
+                heroCardImage.sprite = subType13;
+            }
+            if (subtype == 18)
+            {
+                
+                heroCardImage.sprite = subType18;
+            }
+        }
+
+        if (type == 0)
+        {
+            BuySucces.gameObject.SetActive(false);
+            buyCardbuttom.gameObject.SetActive(false);
+            heroCardImage.sprite = clockSprite;
+        }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
