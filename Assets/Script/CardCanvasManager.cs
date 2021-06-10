a9059bb5d95cd8fef586f043e6121b3490eb44e2
caishen233 <caishen@89trillion.com>
2021-06-10 17:53:01 +0800

    修改了命名和图片变形问题，用了资源动态加载，代码更灵活移动，减少GetComponent使用

---
+++ Assets/Script/CardCanvasManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.UI;
+
+public class CardCanvasManager : MonoBehaviour
+{
+    //现实金币数和钻石数的Text
+    public Text coinSumText;
+    public Text diamondSumText;
+    [SerializeField] private Transform contentTransform;
+    //生成卡片的预设体
+    [SerializeField] private HeroCardItem heroCardPrefab;
+    //判断是否已经生成卡片
+    private bool isCreatePrefab = false;
+    private HeroCardItem HeroCardScriptObject;
+    [SerializeField] private GameObject scrollViewlContent;
+    CardData cardData = new CardData();
+    /// <summary>
+    /// 如果已经生成过了，不再生成器卡片
+    /// 如果卡片不是3的倍数，补上空卡片
+    /// 生成卡片脚本
+    /// 调用脚本里的方法  
+    /// </summary>
+    public void CreateCard()
+    {
+        //因为JSON和需求都没有钻石和金币奖励卡片的奖励大小，所以默认为0，领取免费时不会改变金币和钻石数目
+        RefreshCoinDiamondText();
+        if (isCreatePrefab)
+        {
+            scrollViewlContent.SetActive(true);
+            return;
+        }
+        isCreatePrefab = true;
+        int listLen = AnalyzeJson.cardList.Count;
+        int cardSum = 0;
+        if (listLen % 3 != 0)
+        {
+            cardSum = 3 * (listLen / 3 + 1) - listLen;
+        }
+        for (int i = 0; i < listLen; i++)
+        {
+            Instantiate(heroCardPrefab, contentTransform).Init(AnalyzeJson.cardList[i]);
+        }
+
+        for (int i = 0; i < cardSum; i++)
+        {
+            Instantiate(heroCardPrefab, contentTransform).Init(cardData);
+        }
+    }
+    
+    /// <summary>
+    /// 关闭shop
+    /// </summary>
+    public void CLoseCardView()
+    {
+        scrollViewlContent.SetActive(false);
+    }
+    /// <summary>
+    /// 刷新金币和钻石数目
+    /// type==1代表金币  2代表钻石
+    /// </summary>
+    public void RefreshCoinDiamondText()
+    {
+        coinSumText.text = CoinDiamondManager.Instance.GetCoinDiamond(1).ToString();
+        diamondSumText.text = CoinDiamondManager.Instance.GetCoinDiamond(2).ToString();
+    }
+}
+
+
+
+
+
+
+
---
+++ Assets/Script/HeroCardItem.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class HeroCardItem : MonoBehaviour
+{
+    //紫色卡片底色
+    [SerializeField] private Image purpleBackGround;
+    //卡片的价格text
+    [SerializeField] private Text heroCardCost;
+    //卡片英雄图片Image
+    [SerializeField] private Image heroCardImage;
+    //购买按钮
+    [SerializeField] private Button buyCardButtom;
+    //金币图案
+    [SerializeField] private Image coinIamge;
+    //免费领取字样
+    [SerializeField] private Text freeText;
+    //卡片名字
+    [SerializeField] private Text cardName;
+    //空卡片名字
+    [SerializeField] private Text clockText;
+    //购买成功图案
+    [SerializeField] private Image buySucces;
+    //空卡片显示的图片
+    [SerializeField] private Sprite clockSprite;
+    //金币图片
+    [SerializeField] private Sprite coin;
+    //钻石图片
+    [SerializeField] private Sprite diamond;
+    //四张英雄卡片图片
+    [SerializeField] private Sprite[] subTypeSprite=new Sprite[4];
+    //链表指针
+    private CardData cardData = new CardData();
+    /// <summary>
+    /// 购买之后隐藏button
+    /// 修改金币数和钻石数
+    /// </summary>
+    public void BuyClick()
+    {
+        buyCardButtom.gameObject.SetActive(false);
+        CoinDiamondManager.Instance.ChangeCoinDiamond(cardData.type,cardData.costGold);
+        GameObject.Find("shopCanvas").GetComponent<CardCanvasManager>().RefreshCoinDiamondText();
+    }
+    // Prefab英雄卡片heroCardCost赋值
+    public void Init(CardData cardData)
+    {
+        this.cardData = cardData;
+        clockText.gameObject.SetActive(cardData.type==0);
+        buySucces.gameObject.SetActive(cardData.type!=0);
+        buyCardButtom.gameObject.SetActive(cardData.type!=0);
+        freeText.gameObject.SetActive(cardData.type == 1 || cardData.type == 2);
+        Debug.Log(cardData.type);
+        if (cardData.type < 3)
+        {
+            purpleBackGround.gameObject.SetActive(false);
+            coinIamge.gameObject.SetActive(false);
+            if (cardData.type == 1)
+            {
+                heroCardImage.sprite = coin;
+                cardName.text = "Coins";
+            }
+            else if (cardData.type == 2)
+            {
+                heroCardImage.sprite = diamond;
+                cardName.text = "Diamond";
+            }
+            else
+            {
+                //空卡片
+                heroCardImage.sprite = clockSprite;
+            }
+        }
+        else
+        {
+            //英雄卡片
+            heroCardCost.text = cardData.costGold.ToString();
+            cardName.text = "Viking Wattion";
+            if (cardData.subType == 7) heroCardImage.sprite = subTypeSprite[0];
+            else if (cardData.subType == 13) heroCardImage.sprite = subTypeSprite[1];
+            else if (cardData.subType == 18) heroCardImage.sprite = subTypeSprite[2];
+            else  heroCardImage.sprite = subTypeSprite[3];
+        }
+        heroCardImage.GetComponent<Image>().SetNativeSize();
+    }
+    /// <summary>
+    /// 动态获取四个卡片的图片
+    /// </summary>
+    private void Awake()
+    {
+        subTypeSprite[0] = Resources.Load<Sprite>("7");
+        subTypeSprite[1] = Resources.Load<Sprite>("13");
+        subTypeSprite[2] = Resources.Load<Sprite>("18");
+        subTypeSprite[3] = Resources.Load<Sprite>("20");
+    }
+}
